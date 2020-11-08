164b56f84ea3bd9c3b4af7c2754f40a4d9b29a26
COMP3220-G12 <68089001+COMP3220-G12@users.noreply.github.com>
2020-11-08 12:58:59 -0500

    added logo and changed nav bar colour

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch, BrowserRouter } from "react-router-dom";
-import Home from './components/Home';
+import AppRoot from './components/AppRoot';
 function App() {
   return (
     <BrowserRouter>
       {/* switch allows switching which components render.  */}
       <Switch>
         {/* route allows you to render by url path */}
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={AppRoot} />
 
       </Switch>
     </BrowserRouter>
