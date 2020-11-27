6a3ef689cab3a59ef22de26ed7abc2d8b910cdfc
bsohail <bmsohail@hotmail.com>
2020-11-26 21:10:35 -0500

    Created Actions workflow and test cases

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Route, Switch, BrowserRouter } from "react-router-dom";
-import Home from './components/Home';
-import Login from './components/Login';
 import Main from './components/Main';
 import Documentation from './components/Documentation';
+import {Button} from './button/button';
+
 function App() {
   return (
+    <div data-testid="button">
+      <Button label="click me please"/>
     <BrowserRouter>
       {/* switch allows switching which components render.  */}
       <Switch>
@@ -15,6 +17,7 @@ function App() {
         <Route exact path="/documentation" component={Documentation}/>
       </Switch>
     </BrowserRouter>
+    </div>
   );
 }
 
---
+++ src/services/firebase.test.js
@@ -0,0 +1,21 @@
+import {
+    signin,
+    signout,
+}from '../store/actions/auth';
+describe('Firebase Test Suite', ()=>{
+    beforeAll(async()=>{
+        await signout();
+    })
+})
+test('signInWithEmailAndPassword should throw error',async()=>{
+    let error='';
+    try{
+        await signin('testing@gmail.com','1');
+    }catch(err){
+        error=err.toString();
+        expect(error).toEqual(
+            'Error: Incorrect Password'
+        );
+    }
+   
+})
