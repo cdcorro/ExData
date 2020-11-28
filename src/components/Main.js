ddd3fb7cafe3afebf5b8dfed684bb5214f6f5607
COMP3220-G12 <68089001+COMP3220-G12@users.noreply.github.com>
2020-11-28 14:18:23 -0500

    Added Catalent logo to Documentation page

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,15 +3,12 @@ import { connect } from "react-redux";
 import Home from "./Home";
 import Login from "./Login";
 import Loader from "./Loader";
-import Header from './Header'
-import Footer from './Footer'
+
 
 const Main = ({ auth }) => {
   return (
     <div>
-    <Header />
       {!auth.isLoaded ? <Loader /> : !auth.isEmpty ? <Home /> : <Login />}
-      <Footer />
     </div>
   );
 };
@@ -21,4 +18,5 @@ function mapStateToProps(state) {
     auth: state.firebaseReducer.auth
   };
 }
+
 export default connect(mapStateToProps)(Main);
